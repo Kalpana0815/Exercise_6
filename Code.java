ad030beb3e92e2ab53d8537f33cbcebe2c0ebfa8
kalpana0815 <kumari.kalpana0815@gmail.com>
2022-11-05 08:45:25 -0400

    Added method sum in code.java.

---
+++ Code.java
@@ -0,0 +1,20 @@
+class MathOpertations{
+
+		public  static void main(String [] args){
+
+			int a = 10;
+			int b = 30;
+
+			MathOperations mo = new MathOperations();
+			int addRes = mo.sum();
+			System.out.println(addRes);
+
+		}
+
+			public int sum(int a, int b){
+
+				return a+b;
+
+		}
+
+}
